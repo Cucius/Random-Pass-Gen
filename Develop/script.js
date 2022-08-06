e634f9411c3f4caeb641110db50b8937a10f5c64
Cucius <Rylan19@Gmail.com>
2022-08-05 22:23:11 -0600

    Added Password length and reformatted

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -15,68 +14,69 @@ generateBtn.addEventListener("click", writePassword);
 
 //arrays
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var  lowerCase = alphabet.split("");
+var lowerCase = alphabet.split("");
 // console.log(lowerCase);
 var upperCase = alphabet.toUpperCase().split("");
 // console.log(upperCase);
-var numbers = [0,1,2,3,4,5,6,7,8,9] ;
-var characters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~',"'"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var characters = ["!", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "'"];
 
-var finalPassL = [];
-var finalPassU = [];
-var finalPassNum = [];
-var finalPassSp = [];
 var finalPass = [];
 
-generatePassword = function() {
-
-//confirm for lowercase, Uppercase, Numeric and Special characters
-var lowCase = window.confirm("Do you want lowercase letters?")
-    if(lowCase){
-    finalPass = finalPassL.concat(lowerCase);
-    console.log(finalPass);
+generatePassword = function () {
+  //confirm for lowercase, Uppercase, Numeric and Special characters
+  var lowCase = window.confirm("Do you want lowercase letters?");
+  if (lowCase) {
+    finalPass = finalPass.concat(lowerCase);
+    // console.log(finalPassL);
     //Lower case added
+
     var upCase = window.confirm("Do you want uppercase Letters?");
-      if (upCase){
-      finalPass = finalPassU.concat(upperCase);
-      console.log(finalPass);
+    if (upCase) {
+      finalPass = finalPass.concat(upperCase);
+      // console.log(finalPass);
     } //Numbers added
     var numVal = window.confirm("Do you want Numbers?");
-      if (numVal){
-      finalPass = finalPassNum.concat(numbers);
-      console.log(finalPass);
-    }//Special Characters
+    if (numVal) {
+      finalPass = finalPass.concat(numbers);
+      // console.log(finalPass);
+    } //Special Characters
     var spChar = window.confirm("Do you want special Characters?");
-    if (spChar){
-    finalPass = finalPassSp.concat(characters);
-    console.log(finalPass + "~");
+    if (spChar) {
+      finalPass = finalPass.concat(characters);
+      // console.log(finalPass);
     }
-    //upper case added without lowerCase
-    else if(!lowCase) {
-      var upCase = window.confirm("Do you want uppercase Letters?");
-      if (upCase){
-      finalPass = finalPassU.concat(upperCase);
+  }
+  //upper case added without lowerCase
+  else if (!lowCase) {
+    var upCase = window.confirm("Do you want uppercase Letters?");
+    if (upCase) {
+      finalPass = finalPass.concat(upperCase);
       console.log(finalPass);
-
-      } 
+    }
     //numbers added without upperCase
-    else if(!upCase) {
+    else if (!upCase) {
       var numVal = window.confirm("Do you want Numbers?");
-      if (numVal){
-      finalPass = finalPassNum.concat(numbers);
-      console.log(finalPass);
+      if (numVal) {
+        finalPass = finalPass.concat(numbers);
+        console.log(finalPass);
+      }
+      //special characters added without numbers
+      else if (!numVal) {
+        var spChar = window.confirm("Do you want special Characters?");
+        if (spChar) {
+          finalPass = finalPass.concat(characters);
+          console.log(finalPass);
+        }
+      }
+    }
+  }
+  var passwordLength = window.prompt("How long should the password be? (8-128)");
 
-      } 
-    
-    else if(!numVal) {
-      var spChar = window.confirm("Do you want special Characters?");
-      if (spChar){
-      finalPass = finalPassSp.concat(characters);
-      console.log(finalPass + "~");
-      } 
-    
-  }     
+  if (passwordLength > 7 && passwordLength < 129) {
+    console.log(passwordLength);
+  } else {
+    window.alert("Should've chosen between 8-128. Not " + passwordLength + " !");
+    return;
   }
-  }
-}
-}
+};
