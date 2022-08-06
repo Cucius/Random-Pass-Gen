96fee329b74530e96b716ef35c568807da693044
Cucius <Rylan19@Gmail.com>
2022-08-05 23:36:40 -0600

    Finished Code

--- Develop/script.js
+++ Develop/script.js
@@ -21,56 +21,50 @@ var upperCase = alphabet.toUpperCase().split("");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var characters = ["!", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "'"];
 
-var finalPass = [];
+var passChoices = [];
 
 generatePassword = function () {
   //confirm for lowercase, Uppercase, Numeric and Special characters
   var lowCase = window.confirm("Do you want lowercase letters?");
   if (lowCase) {
-    finalPass = finalPass.concat(lowerCase);
-    // console.log(finalPassL);
+    passChoices = passChoices.concat(lowerCase);
     //Lower case added
 
     var upCase = window.confirm("Do you want uppercase Letters?");
     if (upCase) {
-      finalPass = finalPass.concat(upperCase);
-      // console.log(finalPass);
+      passChoices = passChoices.concat(upperCase);
     } //Numbers added
     var numVal = window.confirm("Do you want Numbers?");
     if (numVal) {
-      finalPass = finalPass.concat(numbers);
-      // console.log(finalPass);
+      passChoices = passChoices.concat(numbers);
     } //Special Characters
     var spChar = window.confirm("Do you want special Characters?");
     if (spChar) {
-      finalPass = finalPass.concat(characters);
-      // console.log(finalPass);
+      passChoices = passChoices.concat(characters);
     }
   }
   //upper case added without lowerCase
   else if (!lowCase) {
     var upCase = window.confirm("Do you want uppercase Letters?");
     if (upCase) {
-      finalPass = finalPass.concat(upperCase);
-      console.log(finalPass);
+      passChoices = passChoices.concat(upperCase);
     }
     //numbers added without upperCase
     else if (!upCase) {
       var numVal = window.confirm("Do you want Numbers?");
       if (numVal) {
-        finalPass = finalPass.concat(numbers);
-        console.log(finalPass);
+        passChoices = passChoices.concat(numbers);
       }
       //special characters added without numbers
       else if (!numVal) {
         var spChar = window.confirm("Do you want special Characters?");
         if (spChar) {
-          finalPass = finalPass.concat(characters);
-          console.log(finalPass);
+          passChoices = passChoices.concat(characters);
         }
       }
     }
   }
+  //Set Password Length
   var passwordLength = window.prompt("How long should the password be? (8-128)");
 
   if (passwordLength > 7 && passwordLength < 129) {
@@ -79,4 +73,13 @@ generatePassword = function () {
     window.alert("Should've chosen between 8-128. Not " + passwordLength + " !");
     return;
   }
+  var finalPass = [];
+
+  //radomize password
+  for (passwordLength; passwordLength > 0; passwordLength--) {
+    finalPass.push(passChoices[Math.floor(Math.random() * passChoices.length)]);
+  }
+
+  //send the passwod to display
+  return finalPass.join("");
 };
